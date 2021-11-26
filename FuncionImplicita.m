d451c270b3a181b0d526a118b17397a1f200c82e
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-11-26 06:37:33 -0600

    Add files via upload

---
+++ FuncionImplicita.m
@@ -0,0 +1,27 @@
+% Octave Script
+% Title                 :Tema 2.10: Otro tipo de funciones(FunciónExplicita)
+% Nombre del equipo     :"The Avengers"
+% Integrantes           :Guadalupe Cristel Martinez
+%                       :Ana Cristina Franco Gonzalez
+%                       :Guadalupe Rivera Maldonado
+%                       :Orlando Urbano Trejo
+%                       :Alexandro Perez Aguirre
+%                       :Adriana Trejo Patricio
+% Date                  :25_11_2021
+% Version               :1
+% Usage                 :octave>path/FunciónExplicita
+%                       :Requiere aplicación octave, usar linea de comandos
+%                       :https://octaveintro.readthedocs.io/en/latest/index.htm
+
+clear
+% Dominio de la función
+x=-3:0.1:3
+% Regla de Correspondencia
+fx= x.^2 - 2.*x + 1
+hold on
+grid on;
+plot([-10 10],[0 0],'r-',"linewidth",1,"markersize",3);
+plot([0 0],[-20 20],'r-',"linewidth",1,"markersize",3);
+% Plotear función
+plot (x,fx)
+title(['Función Explicita']);
